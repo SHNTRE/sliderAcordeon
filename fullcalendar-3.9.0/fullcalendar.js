1c0a88e12a382f7d9caa3bfcb9651314bf738481
SHNTRE <116513595+SHNTRE@users.noreply.github.com>
2020-07-16 18:12:14 +0000

    add readme

--- fullcalendar-3.9.0/fullcalendar.js
+++ fullcalendar-3.9.0/fullcalendar.js
@@ -462,4 +462,63 @@ function parseFieldSpecs(input) {
     else if ($.isArray(input)) {
         tokens = input;
     }
-    for (i = 0; i
+    for (i = 0; i < tokens.length; i++) {
+        token = tokens[i];
+        if (typeof token === 'string') {
+            specs.push(token.charAt(0) === '-' ?
+                { field: token.substring(1), order: -1 } :
+                { field: token, order: 1 });
+        }
+        else if (typeof token === 'function') {
+            specs.push({ func: token });
+        }
+    }
+    return specs;
+}
+exports.parseFieldSpecs = parseFieldSpecs;
+function compareByFieldSpecs(obj1, obj2, fieldSpecs, obj1fallback, obj2fallback) {
+    var i;
+    var cmp;
+    for (i = 0; i < fieldSpecs.length; i++) {
+        cmp = compareByFieldSpec(obj1, obj2, fieldSpecs[i], obj1fallback, obj2fallback);
+        if (cmp) {
+            return cmp;
+        }
+    }
+    return 0;
+}
+exports.compareByFieldSpecs = compareByFieldSpecs;
+function compareByFieldSpec(obj1, obj2, fieldSpec, obj1fallback, obj2fallback) {
+    if (fieldSpec.func) {
+        return fieldSpec.func(obj1, obj2);
+    }
+    var val1 = obj1[fieldSpec.field];
+    var val2 = obj2[fieldSpec.field];
+    if (val1 == null && obj1fallback) {
+        val1 = obj1fallback[fieldSpec.field];
+    }
+    if (val2 == null && obj2fallback) {
+        val2 = obj2fallback[fieldSpec.field];
+    }
+    return flexibleCompare(val1, val2) * (fieldSpec.order || 1);
+}
+exports.compareByFieldSpec = compareByFieldSpec;
+function flexibleCompare(a, b) {
+    if (!a && !b) {
+        return 0;
+    }
+    if (b == null) {
+        return -1;
+    }
+    if (a == null) {
+        return 1;
+    }
+    if ($.type(a) === 'string' || $.type(b) === 'string') {
+        return String(a).localeCompare(String(b));
+    }
+    return a - b;
+}
+exports.flexibleCompare = flexibleCompare;
+/* Date Utilities
+----------------------------------------------------------------------------------------------------------------------*/
+exports.dayIDs = ['sun', 'mon
