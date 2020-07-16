17081e5f9f5989cb86ace5d2f4eabf6467efe46c
SHNTRE <116513595+SHNTRE@users.noreply.github.com>
2020-07-16 09:14:01 +0000

    Some clarifications

--- fullcalendar-3.9.0/fullcalendar.js
+++ fullcalendar-3.9.0/fullcalendar.js
@@ -269,4 +269,40 @@ function getOuterRect(el, origin) {
     };
 }
 exports.getOuterRect = getOuterRect;
-// Que
+// Queries the area within the margin/border/scrollbars of a jQuery element. Does not go within the padding.
+// Returns a rectangle with absolute coordinates: left, right (exclusive), top, bottom (exclusive).
+// Origin is optional.
+// WARNING: given element can't have borders
+// NOTE: should use clientLeft/clientTop, but very unreliable cross-browser.
+function getClientRect(el, origin) {
+    var offset = el.offset();
+    var scrollbarWidths = getScrollbarWidths(el);
+    var left = offset.left + getCssFloat(el, 'border-left-width') + scrollbarWidths.left - (origin ? origin.left : 0);
+    var top = offset.top + getCssFloat(el, 'border-top-width') + scrollbarWidths.top - (origin ? origin.top : 0);
+    return {
+        left: left,
+        right: left + el[0].clientWidth,
+        top: top,
+        bottom: top + el[0].clientHeight // clientHeight includes padding but NOT scrollbars
+    };
+}
+exports.getClientRect = getClientRect;
+// Queries the area within the margin/border/padding of a jQuery element. Assumed not to have scrollbars.
+// Returns a rectangle with absolute coordinates: left, right (exclusive), top, bottom (exclusive).
+// Origin is optional.
+function getContentRect(el, origin) {
+    var offset = el.offset(); // just outside of border, margin not included
+    var left = offset.left + getCssFloat(el, 'border-left-width') + getCssFloat(el, 'padding-left') -
+        (origin ? origin.left : 0);
+    var top = offset.top + getCssFloat(el, 'border-top-width') + getCssFloat(el, 'padding-top') -
+        (origin ? origin.top : 0);
+    return {
+        left: left,
+        right: left + el.width(),
+        top: top,
+        bottom: top + el.height()
+    };
+}
+exports.getContentRect = getContentRect;
+// Returns the computed left/right/top/bottom scrollbar widths for the given jQuery element.
+// WARNING: given element can't have borders (which will cause offsetWidth/offsetHeight to be larger
