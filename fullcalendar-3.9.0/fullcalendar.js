e341c4e94a08b512eeba7ba41284d1343378560f
SHNTRE <116513595+SHNTRE@users.noreply.github.com>
2020-11-08 10:46:37 +0000

    incremented version & release

--- fullcalendar-3.9.0/fullcalendar.js
+++ fullcalendar-3.9.0/fullcalendar.js
@@ -2603,4 +2603,39 @@ function populateInstanceComputableOptions(options) {
         }
     });
 }
-exports.populateInstanceCompu
+exports.populateInstanceComputableOptions = populateInstanceComputableOptions;
+// Initialize jQuery UI datepicker translations while using some of the translations
+// Will set this as the default locales for datepicker.
+function datepickerLocale(localeCode, dpLocaleCode, dpOptions) {
+    // get the FullCalendar internal option hash for this locale. create if necessary
+    var fcOptions = exports.localeOptionHash[localeCode] || (exports.localeOptionHash[localeCode] = {});
+    // transfer some simple options from datepicker to fc
+    fcOptions.isRTL = dpOptions.isRTL;
+    fcOptions.weekNumberTitle = dpOptions.weekHeader;
+    // compute some more complex options from datepicker
+    $.each(dpComputableOptions, function (name, func) {
+        fcOptions[name] = func(dpOptions);
+    });
+    var jqDatePicker = $.datepicker;
+    // is jQuery UI Datepicker is on the page?
+    if (jqDatePicker) {
+        // Register the locale data.
+        // FullCalendar and MomentJS use locale codes like "pt-br" but Datepicker
+        // does it like "pt-BR" or if it doesn't have the locale, maybe just "pt".
+        // Make an alias so the locale can be referenced either way.
+        jqDatePicker.regional[dpLocaleCode] =
+            jqDatePicker.regional[localeCode] = // alias
+                dpOptions;
+        // Alias 'en' to the default locale data. Do this every time.
+        jqDatePicker.regional.en = jqDatePicker.regional[''];
+        // Set as Datepicker's global defaults.
+        jqDatePicker.setDefaults(dpOptions);
+    }
+}
+exports.datepickerLocale = datepickerLocale;
+// Sets FullCalendar-specific translations. Will set the locales as the global default.
+function locale(localeCode, newFcOptions) {
+    var fcOptions;
+    var momOptions;
+    // get the FullCalendar internal option hash for this locale. create if necessary
+    fcOptions = exports.localeOptionHash[localeCode] || (expo
