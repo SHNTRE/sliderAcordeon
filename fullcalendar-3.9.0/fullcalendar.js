1e7444b332c9e9da6860e5d08b025926f87cb9eb
SHNTRE <116513595+SHNTRE@users.noreply.github.com>
2020-12-01 16:42:56 +0000

    Release v1.7.0

--- fullcalendar-3.9.0/fullcalendar.js
+++ fullcalendar-3.9.0/fullcalendar.js
@@ -4660,4 +4660,40 @@ function formatRange(date1, date2, formatStr, separator, isRTL) {
     // BTW, this is not important for `formatDate` because it is impossible to put custom tokens
     // or non-zero areas in Moment's localized format strings.
     formatStr = localeData.longDateFormat(formatStr) || formatStr;
- 
+    return renderParsedFormat(getParsedFormatString(formatStr), date1, date2, separator || ' - ', isRTL);
+}
+exports.formatRange = formatRange;
+/*
+Renders a range with an already-parsed format string.
+*/
+function renderParsedFormat(parsedFormat, date1, date2, separator, isRTL) {
+    var sameUnits = parsedFormat.sameUnits;
+    var unzonedDate1 = date1.clone().stripZone(); // for same-unit comparisons
+    var unzonedDate2 = date2.clone().stripZone(); // "
+    var renderedParts1 = renderFakeFormatStringParts(parsedFormat.fakeFormatString, date1);
+    var renderedParts2 = renderFakeFormatStringParts(parsedFormat.fakeFormatString, date2);
+    var leftI;
+    var leftStr = '';
+    var rightI;
+    var rightStr = '';
+    var middleI;
+    var middleStr1 = '';
+    var middleStr2 = '';
+    var middleStr = '';
+    // Start at the leftmost side of the formatting string and continue until you hit a token
+    // that is not the same between dates.
+    for (leftI = 0; leftI < sameUnits.length && (!sameUnits[leftI] || unzonedDate1.isSame(unzonedDate2, sameUnits[leftI])); leftI++) {
+        leftStr += renderedParts1[leftI];
+    }
+    // Similarly, start at the rightmost side of the formatting string and move left
+    for (rightI = sameUnits.length - 1; rightI > leftI && (!sameUnits[rightI] || unzonedDate1.isSame(unzonedDate2, sameUnits[rightI])); rightI--) {
+        // If current chunk is on the boundary of unique date-content, and is a special-case
+        // date-formatting postfix character, then don't consume it. Consider it unique date-content.
+        // TODO: make configurable
+        if (rightI - 1 === leftI && renderedParts1[rightI] === '.') {
+            break;
+        }
+        rightStr = renderedParts1[rightI] + rightStr;
+    }
+    // The area in the middle is different for both of the dates.
+    // Collect them distinctly 
