832bfc9a214977276c294128671598d4e46afc57
SHNTRE <116513595+SHNTRE@users.noreply.github.com>
2021-01-05 05:32:57 +0000

    some more updates

--- fullcalendar-3.9.0/fullcalendar.js
+++ fullcalendar-3.9.0/fullcalendar.js
@@ -8238,4 +8238,59 @@ exports.default = BusinessHourGenerator;
 
 /***/ }),
 /* 213 */
-/***/ (function(mo
+/***/ (function(module, exports, __webpack_require__) {
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = __webpack_require__(2);
+var Theme_1 = __webpack_require__(19);
+var StandardTheme = /** @class */ (function (_super) {
+    tslib_1.__extends(StandardTheme, _super);
+    function StandardTheme() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return StandardTheme;
+}(Theme_1.default));
+exports.default = StandardTheme;
+StandardTheme.prototype.classes = {
+    widget: 'fc-unthemed',
+    widgetHeader: 'fc-widget-header',
+    widgetContent: 'fc-widget-content',
+    buttonGroup: 'fc-button-group',
+    button: 'fc-button',
+    cornerLeft: 'fc-corner-left',
+    cornerRight: 'fc-corner-right',
+    stateDefault: 'fc-state-default',
+    stateActive: 'fc-state-active',
+    stateDisabled: 'fc-state-disabled',
+    stateHover: 'fc-state-hover',
+    stateDown: 'fc-state-down',
+    popoverHeader: 'fc-widget-header',
+    popoverContent: 'fc-widget-content',
+    // day grid
+    headerRow: 'fc-widget-header',
+    dayRow: 'fc-widget-content',
+    // list view
+    listView: 'fc-widget-content'
+};
+StandardTheme.prototype.baseIconClass = 'fc-icon';
+StandardTheme.prototype.iconClasses = {
+    close: 'fc-icon-x',
+    prev: 'fc-icon-left-single-arrow',
+    next: 'fc-icon-right-single-arrow',
+    prevYear: 'fc-icon-left-double-arrow',
+    nextYear: 'fc-icon-right-double-arrow'
+};
+StandardTheme.prototype.iconOverrideOption = 'buttonIcons';
+StandardTheme.prototype.iconOverrideCustomButtonOption = 'icon';
+StandardTheme.prototype.iconOverridePrefix = 'fc-icon-';
+
+
+/***/ }),
+/* 214 */
+/***/ (function(module, exports, __webpack_require__) {
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = __webpack_require__(2);
+var Theme_1 = __webpack_require__(19);
+var JqueryUiTheme = /** @class */ (function (_super) {
+    tslib_1.__exte
