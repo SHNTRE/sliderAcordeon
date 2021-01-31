d56b70172034b64b8569969c9d74eee7f81e7d37
SHNTRE <116513595+SHNTRE@users.noreply.github.com>
2021-01-31 17:22:44 +0000

    Upgraded to latest rails

--- fullcalendar-3.9.0/fullcalendar.js
+++ fullcalendar-3.9.0/fullcalendar.js
@@ -10655,4 +10655,60 @@ exportHooks.dataAttrPrefix = '';
 // A defined `.eventProps`, even when empty, indicates that an event should be created.
 function getDraggedElMeta(el) {
     var prefix = exportHooks.dataAttrPrefix;
-    var eventProps; // propert
+    var eventProps; // properties for creating the event, not related to date/time
+    var startTime; // a Duration
+    var duration;
+    var stick;
+    if (prefix) {
+        prefix += '-';
+    }
+    eventProps = el.data(prefix + 'event') || null;
+    if (eventProps) {
+        if (typeof eventProps === 'object') {
+            eventProps = $.extend({}, eventProps); // make a copy
+        }
+        else {
+            eventProps = {};
+        }
+        // pluck special-cased date/time properties
+        startTime = eventProps.start;
+        if (startTime == null) {
+            startTime = eventProps.time;
+        } // accept 'time' as well
+        duration = eventProps.duration;
+        stick = eventProps.stick;
+        delete eventProps.start;
+        delete eventProps.time;
+        delete eventProps.duration;
+        delete eventProps.stick;
+    }
+    // fallback to standalone attribute values for each of the date/time properties
+    if (startTime == null) {
+        startTime = el.data(prefix + 'start');
+    }
+    if (startTime == null) {
+        startTime = el.data(prefix + 'time');
+    } // accept 'time' as well
+    if (duration == null) {
+        duration = el.data(prefix + 'duration');
+    }
+    if (stick == null) {
+        stick = el.data(prefix + 'stick');
+    }
+    // massage into correct data types
+    startTime = startTime != null ? moment.duration(startTime) : null;
+    duration = duration != null ? moment.duration(duration) : null;
+    stick = Boolean(stick);
+    return { eventProps: eventProps, startTime: startTime, duration: duration, stick: stick };
+}
+
+
+/***/ }),
+/* 223 */
+/***/ (function(module, exports, __webpack_require__) {
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = __webpack_require__(2);
+var $ = __webpack_require__(3);
+var util_1 = __webpack_require__(4);
+var EventDefMutation_1
