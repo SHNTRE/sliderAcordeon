8d4ff141ab6f781e35b1fd0516780dcc15028525
SHNTRE <116513595+SHNTRE@users.noreply.github.com>
2022-11-20 20:30:04 +0000

    true

---
+++ fullcalendar-3.9.0/locale/ru.js
@@ -0,0 +1,1 @@
+!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t(require("moment"),require("fullcalendar")):"function"==typeof define&&define.amd?define(["moment","fullcalen
