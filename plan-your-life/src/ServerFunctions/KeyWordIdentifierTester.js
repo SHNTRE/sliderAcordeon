7b2760bfb9db11906ac2d6763a606a67f7a57181
SHNTRE <116513595+SHNTRE@users.noreply.github.com>
2023-03-05 08:16:14 +0000

    Update default.py

--- plan-your-life/src/ServerFunctions/KeyWordIdentifierTester.js
+++ plan-your-life/src/ServerFunctions/KeyWordIdentifierTester.js
@@ -54,4 +54,67 @@ describe("KeyWordIdentifierTest", function() {
 			'newList': 'false',
 			'add': 'false',
 			'download': 'false',
-			'remind':
+			'remind': 'true',
+			'remove': 'false',
+			'destination': '',
+			'item': '',
+			'time': '',
+			'where': 'school',
+			'event': 'pick up tom',
+			'duration': '',
+		};
+		var kwiEvent2 = KeyWordIdentifier.KWI("remind me to pick up tom from school");
+		assert.equal(JSON.stringify(intendedEvent2), JSON.stringify(kwiEvent2));
+		done();	
+	});
+	it("create JSON for basic reminder \"remind me to pick up tom from school at 7\"", function(done) {
+		var intendedEvent3 = {
+			'newList': 'false',
+			'add': 'false',
+			'download': 'false',
+			'remind': 'true',
+			'remove': 'false',
+			'destination': '',
+			'item': '',
+			'time': '7',
+			'where': 'school',
+			'event': 'pick up tom',
+			'duration': '',
+		};
+		var kwiEvent3 = KeyWordIdentifier.KWI("remind me to pick up tom from school at 7");
+		assert.equal(JSON.stringify(intendedEvent3), JSON.stringify(kwiEvent3));
+		done();
+	});
+	it("create JSON for basic reminder \"remind me to go running for 30 minutes on saturday - must check manually to ensure date is correct\"", function(done) {
+		var intendedEvent4 = {
+			'newList': 'false',
+			'add': 'false',
+			'download': 'false',
+			'remind': 'true',
+			'remove': 'false',
+			'destination': '',
+			'item': '',
+			'time': 'saturday',
+			'where': '',
+			'event': 'go running',
+			'duration': '30 minutes',
+		};
+		var kwiEvent4 = KeyWordIdentifier.KWI("remind me to go running for 30 minutes on saturday");
+		assert.equal(JSON.stringify(intendedEvent4), JSON.stringify(kwiEvent4));
+		done();		
+	});
+	it("check for error when remind keyword is not used \"pick up tom\"", function(done) {
+		try {
+		KeyWordIdentifier.KWI("pick up tom");
+		}
+		catch(err){
+			assert.equal('Error: No keyword provided.', err.toString());
+			done();	
+		}	
+	});
+	it("check for error when no content is provided \"remind me to\"", function(done) {
+		try {
+		KeyWordIdentifier.KWI("remind me");
+		}
+		catch(err){
+		
