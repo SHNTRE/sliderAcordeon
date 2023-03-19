b1ba60b237c4ffc78ee00adc92bd88ed7a55aa74
SHNTRE <116513595+SHNTRE@users.noreply.github.com>
2023-03-19 22:33:19 +0000

    ggg

--- plan-your-life/src/app/app.module.ts
+++ plan-your-life/src/app/app.module.ts
@@ -3,4 +3,38 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {MAT_DIALOG_DEFAULT_OPTIONS, MatFormFieldModule, MatDialogModule, MatInputModule} from '@angular/material';
-import { BrowserAnimationsMod
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { AppComponent } from './app.component';
+import { UserPromptComponent } from './user-prompt/user-prompt.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { AppSignInComponent, AuthKeyPopup } from './app-sign-in/app-sign-in.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserPromptComponent,
+    EventListComponent,
+    AppSignInComponent,
+    AuthKeyPopup,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatInputModule,
+    BrowserAnimationsModule
+  ],
+  providers: [
+    {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {hasBackdrop: false}},
+    
+  ],
+  entryComponents: [
+    AuthKeyPopup
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+construcor(){} }
---
+++ plan-your-life/src/app/event-list/event-list.component.ts
@@ -0,0 +1,3 @@
+import { Component, OnInit } from '@angular/core';
+import { PylService } from '../planyourlife.service';
+import { HttpClient } from '@angular/common/http';
